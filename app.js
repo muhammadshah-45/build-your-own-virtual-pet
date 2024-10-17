2e6d273fb3568402669387ec0a5a4cb47298d963
muhammadshah-45 <mshahaurangzeb45@gmail.com>
2024-10-17 10:57:27 +0500

    Updated type,feed operations

--- app.js
+++ app.js
@@ -2,11 +2,11 @@
 
 
 var pet = {
-    name:"Poppy",
-    type: "dog",
+    name:prompt("What is your pet name?"),
+    type:prompt("What type of your pet?"),
     age: 3,
-    happiness:30,
-    hunger:45,
+    happiness:+prompt("how happy is your pet between(0-100)"),
+    hunger:+prompt("how hunger is your pet between(0-100)"),
     
     //Add Methods 
 
@@ -14,7 +14,6 @@ var pet = {
         this.hunger -= 20; //Decrease hunger by 20
         if(this.hunger< 0){
            this.hunger = 0; // hunger doesn't go below 0
-
         }
         alert(`${this.name} has been fed! the hunger level is now ${this.hunger}.`);
    },
@@ -32,10 +31,11 @@ var pet = {
        if(this.happiness > 0){
            this.happiness = 0;
        }
+
        if(this.hunger <100){
            this.hunger = 100;
        }
-       alert(`${this.name} has aged! Age:${this.age}, happiness:${this.happiness}, Hunger:${this.hunger}`)
+       alert(`${this.name} has aged! Age:${this.age}, Happiness:${this.happiness}, Hunger:${this.hunger}`)
    }
 
 
@@ -44,28 +44,33 @@ var pet = {
 
 function interactive(){
 
-    
     while(true){
     //Ask from user what they want to do
 
-    let action = prompt("What would you like to do? (feed,age,play,etc...");
+    let action = prompt("What would you like to do? (feed,type,agePet,play,exit)");
     if(action === "feed"){
         pet.feed();
         
-    }else if(action === "play"){
+    }else if(action === "name"){
+        alert(`${this.name}`);
+    }
+    else if(action === "play"){
         pet.play();
-    }else if(action === "age"){
+    }else if(action === "agePet"){
         pet.agePet();
+
         
-    }else if(action === "exit"){
+    } else if(action === "type"){
+        alert(`your pet type is ${pet.type}`)
+
+    }
+    else if(action === "exit"){
         alert("thanks for taking care of your pet!");
         break;
     }else{
         alert("Invalid action.Please choose another action")
     }
-}
-
-}
+}}
 
 
 interactive()
